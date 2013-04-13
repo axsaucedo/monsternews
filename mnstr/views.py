b6ec8c2bc7c4b437de841e9b5a2400ce941b07a8
axsauze <axsauze@gmail.com>
2013-04-13 16:20:34 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	mnstr/views.py

--- mnstr/views.py
+++ mnstr/views.py
@@ -7,3 +7,12 @@ def monsterHome(request):
     topic = topics[0]
     comments = Comment.objects.filter(topic = topic, parent=None).order_by("-votes")[:5]
     return render(request, "comments.html", {'comments': comments })
+
+def home(request):
+    allTopics = Topic.objects.all()
+
+    topics = []
+    for topic in allTopics:
+        topics.append({ 'topic' : topic ,'news': News.objects.filter(topic=topic) })
+
+    return render(request, "main.html", { 'topics' : topics })
