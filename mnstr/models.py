77c0fce6423e882266ea8a8456aa440a829c34b3
Anton Smyrnov <as8g10@gmail.com>
2013-04-13 11:39:56 +0100

    model changes

--- mnstr/models.py
+++ mnstr/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    
+class News(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.TextField()
+    image_link = models.TextField()
+    topic = models.ForeignKey(Topic)
+
+class Comment(models.Model):
+    username = models.CharField(max_length=255, null=True)
+    content = models.CharField(max_length=255)
+    time = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey("self", null=True, related_name="children")
+    votes = models.IntegerField(default=0)
--- monsternews/settings.py
+++ monsternews/settings.py
@@ -18,8 +18,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'monsterdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'monster',
-        'PASSWORD': 'monster',
+        'USER': 'root',
+        'PASSWORD': 'romanton',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
