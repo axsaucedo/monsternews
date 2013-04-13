9ede284f4d291122135d2b2894015114798efd73
Anton Smyrnov <as8g10@gmail.com>
2013-04-13 13:29:45 +0100

    model bugfix

--- mnstr/views.py
+++ mnstr/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
 from django.shortcuts import render
+from mnstr.models import *
 
 def monsterHome(request):
+    topics = Topic.objects.all()[:1]
+    comments = Comment.objects.filter(topic_id = topics[0].id)
     return render(request, "main.html")
